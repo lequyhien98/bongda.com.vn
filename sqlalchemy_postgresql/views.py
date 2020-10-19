380a38a8da941246ac65c1a00c9895aa2feec4a8
oem <lequyhien29081998@gmail.com>
2020-10-19 10:51:08 +0700

    update views.py

--- sqlalchemy_postgresql/views.py
+++ sqlalchemy_postgresql/views.py
@@ -1,12 +1,11 @@
-from pprint import pprint
-
 import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from token_genertion.cdn_token_genertion import token_cdn
 from sqlalchemy_postgresql.config import DATABASE_URI
 from sqlalchemy_postgresql.model import Base, Category, Article
-from token_genertion import token
+from token_genertion.ghost_token_genertion import token
 
 engine = create_engine(DATABASE_URI)
 
@@ -21,20 +20,14 @@ def create_tables():
     Base.metadata.create_all(engine)
 
 
-def create_category(category_title):
-    category = session.query(Category).filter(Category.title == category_title).first()
-    if category:
-        return
-    # Create
-    category = Category(title=category_title)
-    session.add(category)
-    session.commit()
-    session.close()
+def recreate_tables():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
 
 def check_article(url):
     print(url)
-    article = session.query(Article).filter(Article.url == url, Article.removed == False).first()
+    article = session.query(Article).filter(Article.url == url).first()
     if article:
         print('Bài này đã tồn tại!')
         return True
@@ -42,9 +35,21 @@ def check_article(url):
         return False
 
 
+def create_category(category_name):
+    category = session.query(Category).filter(Category.name == category_name).first()
+    if category:
+        return
+    # Create category
+    category = Category(name=category_name)
+    session.add(category)
+    session.commit()
+    print('Lưu tag vào database thành công!')
+    session.close()
+
+
 def create_article(title, url, published_at, og_image_url, og_image_path, image_urls, image_paths, excerpt, html,
                    category_name):
-    category_id = session.query(Category).filter(Category.title == category_name).first().id
+    category_id = session.query(Category).filter(Category.name == category_name).first().id
     article = Article(
         title=title,
         url=url,
@@ -59,7 +64,7 @@ def create_article(title, url, published_at, og_image_url, og_image_path, image_
     )
     session.add(article)
     session.commit()
-    print('Lưu vào database thành công!')
+    print('Lưu bài post vào database thành công!')
     session.close()
 
 
@@ -73,35 +78,49 @@ def uploading_image(_og_image_path):
     files = [
         ('file', open(_og_image_path, 'rb'))
     ]
-    headers = {
-    }
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    response = requests.request("POST", url, data=payload, files=files)
     if response.ok:
         _og_image_url = response.json()['original']['url']
+        print('Uploading image is successful')
+        # verify_upload(response.json()['verifyCode'])
+    else:
+        print('Uploading image is not successful')
     return _og_image_url
+
+
+def verify_upload(verify_code):
+    url = "https://cdn1.codeprime.net/api/verify/"
+    headers = {'Authorization': 'JWT {}'.format(token_cdn)}
+    payload = {
+        'verify_code': verify_code,
+    }
+    response = requests.post(url, data=payload, headers=headers)
+    if response.ok:
+        print('Verifying is successful')
+    else:
+        print('Verifying is not successful')
 
 
 def create_article_in_web(title, _og_image_url, excerpt, html, category_name):
     # Make an authenticated request to create a post
+    _id = None
     headers = {'Authorization': 'Ghost {}'.format(token.decode())}
     url = 'https://www.bongdaxanh.com/ghost/api/v3/admin/posts/?source=html'
     body = {'posts': [{'title': title,
-                       'og_title': title,
-                       'twitter_title': title,
+                       'html': html.replace('&nbsp;', '').strip(),
                        'feature_image': _og_image_url,
-                       'tags': [category_name],
-                       'og_image': _og_image_url,
-                       'twitter_image': _og_image_url,
-                       'og_description': excerpt,
-                       'twitter_description': excerpt,
                        'custom_excerpt': excerpt,
                        'excerpt': excerpt,
-                       'html': html
+                       'og_image': _og_image_url,
+                       'og_title': title,
+                       'og_description': excerpt,
+                       'twitter_image': _og_image_url,
+                       'twitter_title': title,
+                       'twitter_description': excerpt,
+                       "meta_title": title,
+                       "meta_description": excerpt,
+                       'tags': [category_name],
                        }]}
     response = requests.post(url, json=body, headers=headers)
     print(response)
 
-
-def recreate_tables():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
